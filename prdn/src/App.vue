1adcdccdc632c782966df7f83980fb1532f28a71
Emmanuel Ramos <Emmanuel.Ramos2@ibm.com>
2018-10-22 23:08:49 -0400

    Website Boilerplate
    Created Website Boilerplate
    SideMenu components

---
+++ prdn/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    
+    <side-menu :sideMenuItems="sideMenuItems"></side-menu>
+    
+  </div>
+</template>
+
+<script>
+import SideMenu from "./components/SideMenu.vue";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      sideMenuItems: ["This is a test"]
+    };
+  },
+  components: {
+    sideMenu: SideMenu
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ prdn/src/components/SideMenu.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-sm-4">
+        <side-menu-item 
+            v-for="(menuItem, index) in sideMenuItems"
+            :key="index"
+        >
+                {{ menuItem }}
+        </side-menu-item>
+    </div>
+</template>
+
+<script>
+import SideMenuItem from "./SideMenuItem.vue";
+
+export default {
+  props: ["sideMenuItems"],
+  components: {
+    sideMenuItem: SideMenuItem
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
