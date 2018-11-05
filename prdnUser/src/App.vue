f3cdecbdba2646894033b0e69b151da50e6b3d52
Carlos Rodriguez <carlos.rodriguez75@upr.edu>
2018-11-04 19:01:52 -0500

    Administrator Control Panel
    Administrator Control Panel:
    Material
    Processes
    Services
    Businesses
    Connections
    Tag System
    Pending Request

---
+++ prdnUser/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div><account></account></div>
+ 
+</template>
+
+<script>
+import Account from "./components/account/Account.vue";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap";
+
+export default {
+  name: "app",
+  components: {
+    account: Account
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
