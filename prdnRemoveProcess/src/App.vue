8f91b8e7676ad2b22b6e342c6d93e608250361ad
Carlos Rodriguez <carlos.rodriguez75@upr.edu>
2018-11-06 22:58:58 -0500

    Admin - Remove Features

---
+++ prdnRemoveProcess/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div><removeprocess></removeprocess></div>
+ 
+</template>
+
+<script>
+import RemoveProcess from "./components/administrator/RemoveProcess.vue";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap";
+
+export default {
+  name: "app",
+  components: {
+    removeprocess: RemoveProcess
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
