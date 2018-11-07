0a8a0658e8d6250724ef31f58b034a7cc9649f2c
Carlos Rodriguez <carlos.rodriguez75@upr.edu>
2018-11-07 00:13:41 -0500

    Admin - Edit and Pending Requests Features

---
+++ prdnEditBusinesses/src/components/administrator/EditBusinesses.vue
@@ -0,0 +1,129 @@
+<template>
+
+<div class="container" id="editBsnPage">
+
+    <div class="row marginTop">
+        <h2><span class="glyphicon glyphicon-edit"></span> Edit Business</h2>
+    </div>
+    <div id="error"></div>
+    <form name="editBsnForm">
+    <div class="row">
+        <div><input type="text" style="display: none" name="companyId" id="companyId"></div>
+        <div class="col-md-6"><input type="text" name="companyName" placeholder="Business Name" id="editBsnName" ></div>
+        <div class="col-md-6"><input type="text" name="website"  placeholder="Website" id="editBsnWebsite"></div>
+        <div class="col-md-6"><input type="text" name="address" placeholder="Address" id="editBsnAddress"></div>
+        <div class="col-md-2"><input type="text" name="city" placeholder="City" id="editBsnCity"></div>
+        <div class="col-md-2"><input type="text" name="country" placeholder="Country" id="editBsnCountry"></div>
+        <div class="col-md-2"><input type="text" name="zipcode" maxlength="5" placeholder="Zip Code" id="editBsnZipCode"></div>
+        <div class="col-md-4"><input type="text" name="phone" maxlength="10" placeholder="Telephone" id="editBsnTelephone"></div>
+        <div class="col-md-4"><input type="email" name="email"  placeholder="Email" id="editBsnEmail"></div>
+        <div class="col-md-4"><input type="text" name="videoURL"  placeholder="Video URL" id="editBsnVideoURL"></div>
+        <div class="col-md-12"><textarea class="form-control textArea" name="description" placeholder="Description" rows="7" id="editBsnDescription"></textarea></div>
+    </div>
+
+
+    <!-- <div class="row">
+         <div class="col-md-12">
+             <h3>Business Photos</h3>
+             <div id="dropzone-edit-photos" class="dropzone"></div>
+         </div>
+    </div>
+
+    <div class="row">
+         <div class="col-md-6">
+             <h3>Business Logo</h3>
+             <div id="dropzone-edit-logo" class="dropzone"></div>
+         </div>
+    </div>
+
+    <div class="row">
+            <div class="col-md-4">
+                <h3>Materials</h3>
+                <ul class="list-group categoryFont navList" id="editBusnMat">
+                </ul>
+            </div>
+
+            <div class="col-md-4">
+                <h3>Processes</h3>
+                <ul class="list-group categoryFont navList" id="editBusnProc">
+                </ul>
+            </div>
+
+            <div class="col-md-4">
+                <h3>Services</h3>
+                <ul class="list-group categoryFont navList" id="editBusnServ">
+                </ul>
+            </div>
+    </div> -->
+
+
+    <div class="row">
+        <div class="col-md-4 col-sm-6 pull-right buttonMargin">
+            <p>
+                <button type="button" class="btn btn-primary btn-lg" onclick="validateEditBsn(this.form)">Edit</button>
+                <button type="button" class="btn btn-default btn-lg" onClick="loadPage('controlPanel')">Cancel</button>
+            </p>
+        </div>
+    </div>
+  </form>
+</div>
+</template>
+<script>
+export default {
+  //JS
+};
+</script>
+<style>
+.myaccount-content {
+  font-size: 16px;
+  line-height: 1.5;
+  border: 1px solid #ededed;
+  padding: 30px;
+}
+
+.myaccount-content form {
+  margin-top: -20px;
+}
+
+@media only screen and (max-width: 575px) {
+  .myaccount-content {
+    padding: 20px 15px;
+  }
+}
+
+.myaccount-content h3 {
+  font-size: 22px;
+  line-height: 1.5;
+  border-bottom: 1px dashed #777;
+  padding-bottom: 10px;
+  margin-bottom: 25px;
+  margin-top: 0;
+}
+
+.myaccount-content .welcome p {
+  color: #000;
+}
+
+.myaccount-content .welcome a {
+  color: #000;
+}
+
+.myaccount-content .welcome a:hover {
+  color: #12a4dd;
+}
+
+.myaccount-content .welcome strong {
+  font-weight: 600;
+  color: #39b0ff;
+}
+
+.container .navbar-header .navbar-toggle {
+  margin-right: 0px;
+}
+.container form h2 {
+  height: 150px;
+}
+.container .marginTop {
+  height: 150px;
+}
+</style>s
---
+++ prdnEditServiceConnections/src/components/administrator/EditServiceConnections.vue
@@ -0,0 +1,106 @@
+<template>
+
+<div class="container listContainer">
+    <div class="row addMarginTop">
+        <h2><span class="glyphicon glyphicon-edit marginTop"></span> Edit Sub-Service Connections</h2>
+    </div>
+
+    <div id="error"></div>
+    <form name="todo">
+    <div class="row">
+        <div class="col-md-4 categoryList">
+            <h4>Services</h4>
+            <div>
+                <select class="form-control" id="editServTypes" onchange="getServiceRelated(this.form)">
+                    <!--<option disabled selected>Choose One Material</option>-->
+                </select>
+            </div>
+
+        </div>
+
+
+            <div class="col-md-4  categoryList">
+                <h5>Materials Connections</h5>
+                <ul class="list-group navList" id="editServMatCons"> </ul>
+            </div>
+
+            <div class="col-md-4 categoryList">
+                <h5>Processes Connections</h5>
+                <ul class="list-group navList" id="editServProcCons">
+                </ul>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 pull-right buttonMargin" id="editServConnBtn">
+                    <p>
+                        <button type="button" class="btn btn-primary btn-lg" id="addBttn" onClick="validateEditServConn(this.form)">Add</button>
+                        <button type="button" class="btn btn-default btn-lg" id="cancelBttn" onClick="loadPage('controlPanel')">Cancel</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </form>
+ </div>
+
+</template>
+<script>
+export default {
+  //JS
+};
+</script>
+<style>
+//CSS
+
+.myaccount-content {
+  font-size: 16px;
+  line-height: 1.5;
+  border: 1px solid #ededed;
+  padding: 30px;
+}
+
+.myaccount-content form {
+  margin-top: -20px;
+}
+
+@media only screen and (max-width: 575px) {
+  .myaccount-content {
+    padding: 20px 15px;
+  }
+}
+
+.myaccount-content h3 {
+  font-size: 22px;
+  line-height: 1.5;
+  border-bottom: 1px dashed #777;
+  padding-bottom: 10px;
+  margin-bottom: 25px;
+  margin-top: 0;
+}
+
+.myaccount-content .welcome p {
+  color: #000;
+}
+
+.myaccount-content .welcome a {
+  color: #000;
+}
+
+.myaccount-content .welcome a:hover {
+  color: #12a4dd;
+}
+
+.myaccount-content .welcome strong {
+  font-weight: 600;
+  color: #39b0ff;
+}
+
+.container .navbar-header .navbar-toggle {
+  margin-right: 0px;
+}
+.container form h2 {
+  height: 150px;
+}
+.container .marginTop {
+  height: 150px;
+}
+</style>s
